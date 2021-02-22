f955ceee88baf632f6008b61f1aa42aa57b19446
ShyamalaPutti <79366300+ShyamalaPutti@users.noreply.github.com>
2021-02-22 19:53:57 +0530

    Add files via upload

---
+++ parentheses.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+		bool checkValidString(string s) 
+		{
+			if(s.length()<1)
+			return true;
+			int balance = 0;
+			for(int i=0;i<s.length();++i){
+				if(s[i] == ')')
+				balance--;
+				else 
+				balance++;
+				if(balance<0)
+				return false;
+			}
+			if(balance==0)
+			return true;
+			balance = 0;
+			for(int i=s.length()-1;i>=0;--i){
+				if(s[i] == '(')
+				balance--;
+				else
+				balance++;
+				if(balance<0)
+				return false;
+			}
+			return true;
+		}
+int main()
+{
+	string s;
+	int i;
+	getline(cin,s);
+	cout<<checkValidString(s);
+}
